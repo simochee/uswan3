8ba1840eea3b9a055936e88c0e96c2627ee9b2b3
Ryoya Tamura <simochee@outlook.jp>
2016-10-20 22:41:23 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+
+module.exports = {
+	watch: true,
+	entry: './src/scripts/entry.js',
+	output: {
+		path: __dirname + '/docs/scripts',
+		filename: 'bundle.js',
+		publicPath: '/docs/'
+	},
+	module: {
+		preLoaders: [
+			{
+				test: /\.tag$/,
+				exclude: /node_modules/,
+				loader: 'riotjs-loader',
+				query: {
+					type: 'babel',
+					template: 'jade'
+				}
+			}
+		],
+		loaders: [
+			{
+				test: /\.js$|\.tag/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.tag']
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.ProvidePlugin({
+			riot: 'riot'
+		})
+	]
+}
